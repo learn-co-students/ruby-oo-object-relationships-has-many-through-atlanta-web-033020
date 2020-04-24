24fd1e0c93626686ea207050d3a8b6758609ed0a
devWRM <mcwen@comcast.net>
2020-04-23 21:04:47 -0400

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,26 @@
+require 'pry'
+
 class Meal
+    attr_accessor :waiter, :customer, :total, :tip
+    @@all = []
+
+    def initialize(waiter, customer, total, tip = 0)
+        @waiter = waiter
+        @customer = customer
+        @total = total
+        @tip = tip
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    
+
 
 end
